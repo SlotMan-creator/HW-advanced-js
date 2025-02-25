a2600d6475a6adfc1033975e1ae5d615cc94ba7a
SlotMan-creator <phoenix5812@yandex.ru>
2025-02-25 21:20:03 +0300

    чутка отформатировали

--- InstaPro/components/posts-page-component.js
+++ InstaPro/components/posts-page-component.js
@@ -1,16 +1,16 @@
-import { USER_POSTS_PAGE } from "../routes.js";
-import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage } from "../index.js";
+import { USER_POSTS_PAGE } from '../routes.js'
+import { renderHeaderComponent } from './header-component.js'
+import { posts, goToPage } from '../index.js'
 
 export function renderPostsPageComponent({ appEl }) {
-  // @TODO: реализовать рендер постов из api
-  console.log("Актуальный список постов:", posts);
+    // @TODO: реализовать рендер постов из api
+    console.log('Актуальный список постов:', posts)
 
-  /**
-   * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
-   * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
-   */
-  const appHtml = `
+    /**
+     * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
+     * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
+     */
+    const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
                 <ul class="posts">
@@ -91,19 +91,19 @@ export function renderPostsPageComponent({ appEl }) {
                     </p>
                   </li>
                 </ul>
-              </div>`;
+              </div>`
 
-  appEl.innerHTML = appHtml;
+    appEl.innerHTML = appHtml
 
-  renderHeaderComponent({
-    element: document.querySelector(".header-container"),
-  });
+    renderHeaderComponent({
+        element: document.querySelector('.header-container'),
+    })
 
-  for (let userEl of document.querySelectorAll(".post-header")) {
-    userEl.addEventListener("click", () => {
-      goToPage(USER_POSTS_PAGE, {
-        userId: userEl.dataset.userId,
-      });
-    });
-  }
+    for (let userEl of document.querySelectorAll('.post-header')) {
+        userEl.addEventListener('click', () => {
+            goToPage(USER_POSTS_PAGE, {
+                userId: userEl.dataset.userId,
+            })
+        })
+    }
 }
